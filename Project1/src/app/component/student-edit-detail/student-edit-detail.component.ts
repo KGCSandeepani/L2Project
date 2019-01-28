dbf2f718ce1a4487a0775b86e225b3c282143b1f
unknown <umithindula213@gmail.com>
2019-01-28 13:08:17 +0530

    student-edit-details

--- Project1/src/app/component/student-edit-detail/student-edit-detail.component.ts
+++ Project1/src/app/component/student-edit-detail/student-edit-detail.component.ts
@@ -1,4 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { StuSelectedCompany } from 'src/app/component/Model/StuSelectedCompany';
+import { student } from 'src/app/component/Model/Student';
+import { ReadUnamePswServiceService } from 'src/app/component/Services/read-uname-psw-service.service';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
+import { DataPassService } from 'src/app/component/Services/data-pass.service';
+import {NgForm,FormControl} from '@angular/forms';
+import { NoOfCompany } from '../Model/NoOfCompany';
+import { company } from 'src/app/component/Model/Company';
+import { AdminUpdateStudentService } from '../Services/admin-update-student.service';
+import { GetNoOfCompanyService } from '../Services/get-no-of-company.service';
+import { AdminViewCompanyService } from 'src/app/component/Services/admin-view-company.service';
+import { StudentSelectedCompanyService } from 'src/app/component/Services/student-selected-company.service';
+import { isUndefined, isNull } from 'util';
+import { NgxNotificationService } from 'ngx-notification';
+import { AngularFireStorage , AngularFireStorageReference , AngularFireUploadTask } from 'angularfire2/storage';
 
 @Component({
   selector: 'app-student-edit-detail',
@@ -6,10 +22,144 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./student-edit-detail.component.css']
 })
 export class StudentEditDetailComponent implements OnInit {
+    loger: string ;
+    message : string;
+    student:student;
+    StuSelectedCompany:StuSelectedCompany[];
+    name:string;
+    urlForSave: string;
+    logger: string;
+    noOfCompany : NoOfCompany;
+    num : number ;
+    array : Array<number> =[];
+    company: Array<company>=[];
+    selectedCompany: StuSelectedCompany;
+    interestField : String[] =['Programming','Web Technologies','Mobile Technologies','Network Technologies','Business Analysis','Quality Assuarance','UI/UX Design']
+    interest1 : String;
+    interest2 : String;
+    interest3 : String;  
+    disableButton =true;
+    probar=false;
 
-  constructor() { }
+    constructor(private data : DataPassService,
+                private ngxNotificationService: NgxNotificationService,
+                private afStorage: AngularFireStorage ,
+                private stuCompany : StudentSelectedCompanyService,
+                private updateService : AdminUpdateStudentService,
+                private readCompanyService: AdminViewCompanyService, 
+                private getNoOfCompany : GetNoOfCompanyService, 
+                private readService: ReadUnamePswServiceService,
+                private route : ActivatedRoute,
+                private router : Router) { }
 
   ngOnInit() {
+    
+    this.loger = this.data.getMessage();
+    console.log(this.loger+"in the student view");
+    this.readService.getId(this.loger);
+    this.readData();
+    this.retrieveCom();
+    this.name = this.readService.setId();
+    this.getNoOfCompany.getData()
+    .subscribe(data => {this.noOfCompany = data;
+    this.num=this.noOfCompany.amount;
+   
+    if(this.num==null){
+      this.num=1;
+    }
+    for (let index = 0; index < this.num; index++) {
+      this.array[index]=index+1;         
+    }
+   
+  });
+
+  this.readCompanyService.getData()
+  .subscribe(data => {this.company = data });
   }
+  
+readData(){
+    this.readService.reteriveData()
+    .subscribe((data : student )=> {
+      this.student = data;
+      console.log(this.student);
+   
+   });
+}
+
+retrieveCom(){
+    console.log('in the com');
+    this.readService.reteriveComData()
+    .subscribe((data : StuSelectedCompany[] )=> {
+      this.StuSelectedCompany = data;
+      console.log(this.StuSelectedCompany);
+   
+  });
+}
+getCompany(item: number,companyname: String){
+  console.log(this.loger+" : "+item+" : "+ companyname);
+  this.stuCompany.getStuSelectedCompany(this.loger,item,companyname)
+  .subscribe((data : StuSelectedCompany)=>this.selectedCompany = data);
+  
+}
+getInterest1(i : String){
+  this.interest1=i;
+}
+getInterest2(i : String){
+  this.interest2=i;
+}
+getInterest3(i : String){
+  this.interest3=i;
+}
+
+
+onSubmit(formdata:NgForm){
+  formdata.form.addControl('uploadPdfUrl' , new FormControl(this.urlForSave));
+  
+   console.log(formdata.value.name);
+   console.log(formdata.value , formdata);
+  if(formdata.value.uname==null || formdata.value.email==null || formdata.value.phoneNo==null || 
+    formdata.value.l1s1==null || formdata.value.l1s2==null || formdata.value.l2s1==null || formdata.value.l2s2==null || 
+    formdata.value.cgpa==null){
+      this.sendNotification();
+    }else{
+      this.updateService.updateStudentData(formdata,this.interest1,this.interest2,this.interest3,this.loger)
+      .subscribe(res=>{
+      this.router.navigate(['student/studentAddSuccess']);
+      });
+    }
+  
+}
+
+sendNotification() {
+  this.ngxNotificationService.sendMessage('Please fill all the information before submit', 'dark', 'bottom-right');
+  //dark, light, success, info, warning, danger and none
+  //top-left, top-right, bottom-left, bottom-right and center
+}
+upload(event) {
+  this.sendNotification1();
+  this.probar=true;
+  const filePath = `/student/CVs/`+ this.loger;
+  this.afStorage.upload(filePath, event.target.files[0]).then( ()=> {
+    const ref = this.afStorage.ref(filePath);
+    const downloadUrl = ref.getDownloadURL().subscribe( url => {
+        this.urlForSave = url;
+        console.log("Uploaded "+this.urlForSave);
+        // this.uploadProgress= this.task.percentageChanges();
+        this.probar=false;
+        this.disableButton=false;
+        this.sendNotification2();
+    })
+  }); 
+}
+
+sendNotification1() {
+  this.ngxNotificationService.sendMessage('Wait until upload file', 'dark', 'bottom-right');
+  //dark, light, success, info, warning, danger and none
+  //top-left, top-right, bottom-left, bottom-right and center
+}
+
+sendNotification2() {
+  this.ngxNotificationService.sendMessage('File uploaded', 'dark', 'bottom-right');
+}
 
 }
