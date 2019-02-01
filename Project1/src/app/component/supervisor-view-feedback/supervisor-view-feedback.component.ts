c9a98ec2dc63f6a631eeb53b8ab61c9f8a0192a5
hasinichathu <164124V@uom.lk>
2019-02-01 15:21:04 +0530

    supervisor-view-feedback

--- Project1/src/app/component/supervisor-view-feedback/supervisor-view-feedback.component.ts
+++ Project1/src/app/component/supervisor-view-feedback/supervisor-view-feedback.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from "angularfire2/database"
+import { DataPassService } from '../Services/data-pass.service';
+import { Observable } from 'rxjs';
 
 
 @Component({
@@ -7,10 +10,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./supervisor-view-feedback.component.css']
 })
 export class SupervisorViewFeedbackComponent implements OnInit {
-  
-  constructor() { }
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource;
+  loger: string;// currently logged user
+  items: Observable<any[]>;
+
+  constructor(private db: AngularFireDatabase, 
+    private data: DataPassService) {
+      this.loger = this.data.getMessage();
+
+     }
 
   ngOnInit() {
+    this.loger = this.data.getMessage();
+    this.db.list('feedback', db => db.orderByChild("name").equalTo(this.loger)).valueChanges().subscribe(res => {this.dataSource = res;console.log(res);} );
+
   }
  
   
