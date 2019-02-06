f38f60a18b68417659a3ad04cc6630f8dc7408d4
hasinichathu <164124V@uom.lk>
2019-02-06 19:54:57 +0530

    stu-feedback

--- Project1/src/app/component/feedback/feedback.component.ts
+++ Project1/src/app/component/feedback/feedback.component.ts
@@ -3,37 +3,68 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import * as firebase from 'firebase/app';
 import { DataPassService } from '../Services/data-pass.service';
 import { AngularFireDatabase, AngularFireList } from "angularfire2/database"
+import { AdminreadstaffService } from 'src/app/component/Services/adminreadstaff.service';
+import { staff } from '../Model/Staff';
 
 @Component({
   selector: 'app-feedback',
   templateUrl: './feedback.component.html',
   styleUrls: ['./feedback.component.css']
 })
+
 export class FeedbackComponent implements OnInit {
-  
+  foods: Food[] = [
+    {value: '1st', viewValue: '1st'},
+    {value: '2nd', viewValue: '2nd'},
+    {value: '3rd', viewValue: '3rd'}
+  ];
+  terms: Termm[] = [
+    {value: '0', viewValue: '1'},
+    {value: '1', viewValue: '2'},
+    {value: '2', viewValue: '3'}
+  ];
   name:string;
-  email:string;
-  phone:string;
+  supervisor:string;
+  term:string;
   feedback:string;
+  staff: staff[];
   constructor(private formBuilder: FormBuilder,private db: AngularFireDatabase, 
-    private dataPass: DataPassService) { }
+    private dataPass: DataPassService,private readService: AdminreadstaffService) { }
 
   ngOnInit() {
+    this.name=this.dataPass.getMessage();
+    this.readService.getData()
+    .subscribe(data => this.staff = data);
+    console.log(this.staff);
     
   }
- 
+  setSupervisor(name:string){
+    this.supervisor=name;
+  }
+  setTerm(name:string){
+    this.term=name;
+  }
+  
   saveMessage(){
     var messagesRef = firebase.database().ref('feedback');
-
+// console.log(this.name+this.supervisor+this.term+this.feedback);
     messagesRef.push({
       name: this.name,
-      email:this.email,
-      phone:this.phone,
+      supervisor:this.supervisor,
+      term:this.term,
       feedback:this.feedback
     });
     this.name="";
-    this.email="";
-    this.phone="";
+    this.term="";
+    this.supervisor="";
     this.feedback="";
   }
+}
+export interface Food {
+  value: string;
+  viewValue: string;
+}
+export interface Termm {
+  value: string;
+  viewValue: string;
 }
